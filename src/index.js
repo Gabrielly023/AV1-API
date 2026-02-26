25874dcc8c46d7d9fd60d0f22c6da29bea0d42d3
Gabrielly023 <Gabrielly023>
2026-02-26 10:02:37 -0300

    26/02

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ export const tarefas = [
     {id: 1, titulo: "estudar node", concluida: false},
     {id: 2, titulo:"fazer banco de dados", concluida: false}
 ];
+
+tarefas.push ({
+    id:3,
+    titulo: "estudar prova",
+    concluida: false
+});
+
+console.log(tarefas);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import {tarefas} from "./index.js";
+
 const app = express();
 
 const PORT = 3000;
@@ -7,7 +9,6 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Olá gaby!");
     res.status(200).json(tarefas);
 });
 
@@ -18,4 +19,10 @@ app.listen(PORT, () => {
 
 app.post("/tarefas", (req, res) => {
     const { titulo } = req.body;
+
+    if (!titulo || titulo.trim() === "") {
+        return res.status(400).json({
+            erro:"Título obrigatório."
+        })
+    }
 })
